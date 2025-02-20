141126141bd0196812dc1758fb9f0b701acb4932
Michael <200627081@student.georgianc.on.ca>
2025-02-20 00:17:58 -0500

    Added
    Signs
    Interaction
    Movement

---
+++ Assets/Scripts/InteractableScript/SignInteraction.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignInteraction2D : MonoBehaviour
+{
+    public GameObject textUI;
+    private bool playerInRange = false;
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            textUI.SetActive(!textUI.activeSelf); 
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            textUI.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScript/Movement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Get input (WASD or arrow keys)
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveInput = new Vector2(moveX, moveY).normalized;
+    }
+
+    [System.Obsolete]
+    void FixedUpdate()
+    {
+        rb.velocity = moveInput * moveSpeed;
+    }
+}
