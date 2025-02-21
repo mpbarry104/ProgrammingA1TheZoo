24e2b78fd8c5f5373dd1fa862c9e1217ad902cec
Michael <200627081@student.georgianc.on.ca>
2025-02-21 16:17:12 -0500

    holy fuck i fixed it

--- Assets/Scripts/PlayerScript/PlayerInteraction.cs
+++ Assets/Scripts/PlayerScript/PlayerInteraction.cs
@@ -6,7 +6,6 @@ public class PlayerInteraction : MonoBehaviour
 
     void Update()
     {
-        // When the player presses E and there is an interactable object in range
         if (Input.GetKeyDown(KeyCode.E) && currentInteractable != null)
         {
             currentInteractable.Interact();
@@ -14,17 +13,15 @@ public class PlayerInteraction : MonoBehaviour
         }
     }
 
-    // This method will be triggered when the player enters the trigger area of the octopus
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Octopus"))  // Ensure the Octopus has the "Octopus" tag
+        if (other.CompareTag("Octopus")) 
         {
             currentInteractable = other.GetComponent<IInteractable>();
             Debug.Log("Entered trigger area with octopus.");
         }
     }
 
-    // This method will be triggered when the player leaves the trigger area of the octopus
     void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Octopus"))
